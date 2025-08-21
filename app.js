22c9a9a271dddc7d81ec11d79b19a696692cbbc1
oluwaseun2017 <oluwaseun.learning@gmail.com>
2025-08-21 13:12:00 -0700

    second push

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-alert("WELCOME TO WORLD")
-
-const name= "Tester"
-
-alert("name")
+const day = "wednesday"
+const position = "3"
+const output = "today is wednesday.it's the 3rd day of the week"
+console.log(day)
+console.log(position)
+console.log('today is wednesday.it is the 3rd day of the week ' + position)
